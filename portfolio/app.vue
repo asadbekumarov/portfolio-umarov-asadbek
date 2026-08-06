cb1120e2ec1543fcb83bf94e84b4799bf9543770
Asadbek <asadbekumarov922@gmail.com>
2026-08-06 14:54:51 +0500

    fix: seo

--- portfolio/app.vue
+++ portfolio/app.vue
@@ -23,138 +23,108 @@ const seoData = computed(() => ({
     `https://portfolio-umarov-asadbek.vercel.app${route.path}`,
 }));
 
-useHead({
+useHead(() => ({
   title: seoData.value.title,
 
   htmlAttrs: {
-    lang: seoData.value.currentLocale === "uz"
-      ? "uz-UZ"
-      : "en-US",
+    lang: seoData.value.currentLocale === "uz" ? "uz-UZ" : "en-US",
   },
 
   meta: [
     { charset: "utf-8" },
-
     {
       name: "viewport",
       content: "width=device-width, initial-scale=1",
     },
-
     {
       name: "description",
       content: seoData.value.description,
     },
-
     {
       name: "author",
       content: seoData.value.author,
     },
-
     {
       name: "keywords",
       content: seoData.value.keywords,
     },
-
     {
       name: "robots",
       content: "index, follow",
     },
-
     {
       name: "theme-color",
       content: "#0284c7",
     },
 
-
-    // Open Graph
     {
       property: "og:type",
       content: "website",
     },
-
     {
       property: "og:title",
       content: seoData.value.ogTitle,
     },
-
     {
       property: "og:description",
       content: seoData.value.ogDescription,
     },
-
     {
       property: "og:locale",
-      content:
-        seoData.value.currentLocale === "uz"
-          ? "uz_UZ"
-          : "en_US",
+      content: seoData.value.currentLocale === "uz" ? "uz_UZ" : "en_US",
     },
-
     {
       property: "og:url",
       content: seoData.value.canonicalUrl,
     },
-
     {
       property: "og:image",
       content: seoData.value.ogImage,
     },
-
     {
       property: "og:image:width",
       content: "1200",
     },
-
     {
       property: "og:image:height",
       content: "630",
     },
-
     {
       property: "og:image:alt",
       content: "Asadbek Umarov Portfolio Preview",
     },
 
+    {
+      property: "og:site_name",
+      content: "Asadbek Umarov Portfolio",
+    },
 
-    // Twitter / X
     {
       name: "twitter:card",
       content: "summary_large_image",
     },
-
     {
       name: "twitter:title",
       content: seoData.value.twitterTitle,
     },
-
     {
       name: "twitter:description",
       content: seoData.value.twitterDescription,
     },
-
     {
       name: "twitter:image",
       content: seoData.value.ogImage,
     },
 
-
-    // Additional SEO
     {
       name: "format-detection",
       content: "telephone=no",
     },
-
-    {
-      name: "msapplication-TileColor",
-      content: "#0284c7",
-    },
-
     {
       name: "application-name",
       content: seoData.value.title,
     },
   ],
-
 
   link: [
     {
@@ -162,30 +132,12 @@ useHead({
       type: "image/svg+xml",
       href: "/favicon.svg",
     },
-
     {
       rel: "canonical",
       href: seoData.value.canonicalUrl,
     },
-
-    {
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com",
-    },
-
-    {
-      rel: "preconnect",
-      href: "https://fonts.gstatic.com",
-      crossorigin: "",
-    },
-
-    {
-      rel: "stylesheet",
-      href:
-        "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap",
-    },
   ],
-});
+}));
 
 
 // JSON-LD structured data
